bbd024bfb2099340d35546f31ad4d524ac16cee5
Kian Aliwalas <oliverkian01@gmail.com>
2024-06-06 15:37:16 -0400

    Javadoc Comments and started main

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,26 @@
+/**
+ * Client.Java
+ * -----------------------
+ * Authors:
+ * Kian Aliwalas
+ * Delanie Crews
+ * -----------------------
+ * CNT4504 Summer 2024
+ * Instructor: Scott Kelly
+ * -----------------------
+ * Iterative Socket Server
+ * Due Date 07/05/24
+ * -----------------------
+ * Implement an iterative (single-threaded) server for use in a client-server
+ * Configured to examine, analyze, and study the effects an iterative server
+ * has on the efficiency (average turn-around time) of processing client requests.
+ */
+import java.io.*;
+import java.net.*;
+
 public class Client {
 
+    public static void main(String[] args) {
+
+    }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,32 @@
+/**
+ * Server.Java
+ * -----------------------
+ * Authors:
+ * Kian Aliwalas
+ * Delanie Crews
+ * -----------------------
+ * CNT4504 Summer 2024
+ * Instructor: Scott Kelly
+ * -----------------------
+ * Iterative Socket Server
+ * Due Date 07/05/24
+ * -----------------------
+ * Implement an iterative (single-threaded) server for use in a client-server
+ * Configured to examine, analyze, and study the effects an iterative server
+ * has on the efficiency (average turn-around time) of processing client requests.
+ */
+
+import java.io.*;
+import java.net.*;
+
+/**
+ *
+ */
 public class Server {
 
+public static void main(String[] args){
+
+
+}
+    
 }
