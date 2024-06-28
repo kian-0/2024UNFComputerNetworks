5d7657b4b2870a1a26852b5627298beb3100369e
Kian Aliwalas <oliverkian01@gmail.com>
2024-06-28 00:57:07 -0400

    Got threading system to work

--- src/Client.java
+++ src/Client.java
@@ -18,9 +18,12 @@
 
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Client {
+    //Variable declaration
+    private static final ArrayList<Thread> threadArrayList = new ArrayList<>();
 
     public static void main(String[] args) {
         //User input IP/Port
@@ -35,46 +38,70 @@ public class Client {
         //Try-Catch opening socket to server
         try (Socket socket = new Socket(ip, port)) {
             System.out.println("Connected to " + ip + ":" + port);
-
-            System.out.println("Opening input stream");
-            InputStream inputStream = socket.getInputStream(); //Gets lines from server
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream)); //Reads lines from server
-
-            System.out.println("Opening output stream");
-            OutputStream outputStream = socket.getOutputStream(); //Opens output stream to server
-            PrintWriter writer = new PrintWriter(outputStream, true); //Opens PrintWrite to server
+            socket.close(); //Closing it, so it can be reopened by the threads. This is required just to indicate that the server exists
 
             //Client Menu
+            menu();
             int clientChoice = 9; //Initializes choice and make sure it isn't an option or 0 to go to while
             while (clientChoice != 0) {
-                menu();
 
                 clientChoice = scanner.nextInt();
                 System.out.println("Client choice " + clientChoice); //Debugging
 
-                //TO-DO Ask for how many requests/threads. Make according to deliverables requirements
-                System.out.println("How many requests?");
-                int requests = scanner.nextInt();
+                //Ask for how many requests/threads.
+                int requests = 0;
+                while (requests == 0) { //While loop for amount of threads to be created
+                    System.out.println("How many requests? 1, 5, 10, 15, 20 or 25");
+                    requests = scanner.nextInt();
+                    switch (requests) { //Keeps looping to ensure valid amount
+                        case 1:
+                        case 5:
+                        case 10:
+                        case 15:
+                        case 20:
+                        case 25:
+                            System.out.println("Creating " + requests + " requests.");
+                            break;
 
-                for (int i = 0; i < requests; i++) { //For loop temporary
-                    writer.println(clientChoice); //Sends client choice to server
-                    writer.flush(); //Clears writer (sender)
-
-                    System.out.println(bufferedReader.readLine()); //Debugging prints out returned value from server.
+                        default:
+                            requests = 0;
+                            System.out.println("Invalid request amount. Try again.");
+                            break;
+                    }
                 }
 
+                //Threading Proto-Type
+                //Creates all the threads
+                for (int i = 0; i < requests; i++) {
+                    myThread thread = new myThread();
+                    thread.setValues(ip, port, clientChoice);
+                    threadArrayList.add(thread);
+                }
+
+                //Starts up all the threads
+                for (int i = 0; i < requests; i++) {
+                    long startTime = System.currentTimeMillis();
+                    threadArrayList.get(i).start();
+                    threadArrayList.get(i).join();
+                    long endTime = System.currentTimeMillis();
+
+                    long elapsedTime = endTime - startTime;
+
+                    System.out.println("Time elapsed: " + elapsedTime + "ms");
+                }
+                menu();
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
-        }
 
+        }
     }
 
     /**
      * Displays options when called
      */
-    private static void menu(){
-        System.out.println("\r\n Enter the number of desired request");
+    private static void menu() {
+        System.out.println("\r\nEnter desired operation");
         System.out.println("1 - Date and Time");
         System.out.println("2 - Uptime");
         System.out.println("3 - Memory use");
@@ -83,5 +110,61 @@ public class Client {
         System.out.println("6 - Running processes");
         System.out.println("0 - Exit \r\n");
     }
+}
 
+class myThread extends Thread {
+    private String ip;
+    private int port;
+    private int clientChoice;
+
+
+    @Override
+    public void run() {
+        try (Socket socket = new Socket(ip, port)) {
+            //System.out.println("Connected to " + ip + ":" + port); //For debugging
+
+            //Starting sending to server
+            //System.out.println("Opening input stream");
+            InputStream inputStream = socket.getInputStream(); //Gets lines from server
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream)); //Reads lines from server
+
+            //Starting read back from server
+            //System.out.println("Opening output stream");
+            OutputStream outputStream = socket.getOutputStream(); //Opens output stream to server
+            PrintWriter writer = new PrintWriter(outputStream, true); //Opens PrintWrite to server
+
+            //Sending to server
+            writer.println(clientChoice); //Sends client choice to the server
+
+            //Waits until the buffered reader has something in it
+            while (!bufferedReader.ready()) {
+                //noinspection BusyWait
+                sleep(1);
+            }
+
+            //Receiving from server
+                System.out.println(bufferedReader.readLine()); //Prints out the response from the server
+
+
+            //Closes all the input/output streams
+            writer.close();
+            bufferedReader.close();
+            socket.close(); //Closes socket
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * A constructor? I feel like this is unnecessary, but I am not entirely sure what I'm doing anyway.
+     * @param ip IP of the server
+     * @param port Port of the server
+     * @param clientChoice Number of the operation chosen
+     */
+    public void setValues(String ip, int port, int clientChoice) {
+        this.ip = ip;
+        this.port = port;
+        this.clientChoice = clientChoice;
+
+    }
 }
--- src/Server.java
+++ src/Server.java
@@ -36,46 +36,49 @@ public class Server {
         int port = scanner.nextInt(); //Takes user input port
         scanner.close();
 
+
         //Try-Catch for Server socket
         try (ServerSocket serverSocket = new ServerSocket(port)) //Opens Server
         {
-
-            System.out.println(InetAddress.getLocalHost());
-            System.out.println("Server is listening on port: " + port); //Debugging
-
+            while (true) {
+                System.out.println(InetAddress.getLocalHost());
+                System.out.println("Server is listening on port: " + port); //Debugging
 
                 Socket socket = serverSocket.accept(); //Taking in client connection
                 System.out.println("Client connected");
 
                 System.out.println("Opening input stream");
                 InputStream inputStream = socket.getInputStream(); //Taking Client input
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream); //Reads Client input
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream)); //Reads Client input
 
                 System.out.println("Opening output stream");
                 OutputStream outputStream = socket.getOutputStream(); //Opens output stream to client
                 PrintWriter writer = new PrintWriter(outputStream, true); //Opens PrintWrite to client
 
-                //System.out.println(inputStreamReader.read());
-
-            int clientChoice = 9; //Initializes clientChoice to get the while to work properly
-
-            while (clientChoice != 0) {
-                clientChoice = Character.getNumericValue(inputStreamReader.read()); //Gets client option choice
+                int clientChoice = Character.getNumericValue(bufferedReader.read()); //Gets client option choice
 
                 //System.out.println(clientChoice); //Debugging
                 //writer.println(clientChoice); //Debugging sends value back to client
 
                 switch (clientChoice) {
+                    case -1:
+                        System.out.println("Startup");
+                        break;
+
                     case 1: //Date and Time
-                            date(writer);
+                        System.out.println("1");
+                        writer.println(new Date().toString());
+                        writer.flush();
                         break;
 
                     case 2: //Uptime
-                            uptime(writer);
+                        writer.println(System.nanoTime()); //in nanoseconds? I don't know need verification
+                        writer.flush();
                         break;
 
                     case 3: //Memory Use
-                            memory(writer);
+                        writer.println(Runtime.getRuntime().freeMemory()); //in bits? needs verification
+                        writer.flush();
                         break;
 
                     case 4: //Netstat
@@ -96,29 +99,5 @@ public class Server {
         } catch (Exception e) { //Catches Exception from opening server socket
             System.out.println("Server Exception: " + e.getMessage());
         }
-    }
-
-    /**
-     * Takes in writer object to send date over
-     * @param writer Output date to client
-     */
-    private static void date(PrintWriter writer){
-        writer.println(new Date());
-    }
-
-    /**
-     * Takes in writer to send time device is on? in nanoseconds? I don't know need verification
-     * @param writer Output uptime to client
-     */
-    private static void uptime(PrintWriter writer){
-        writer.println(System.nanoTime());
-    }
-
-    /**
-     * Takes in writer to send free memory? in bits? needs verification
-     * @param writer Output free memory to client
-     */
-    private static void memory(PrintWriter writer){
-        writer.println(Runtime.getRuntime().freeMemory());
     }
 }
